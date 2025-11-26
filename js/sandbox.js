99702aa7ef263121b2477b89332463bd75584199
Christopher <127crodriguezrufino@frhsd.com>
2025-11-26 11:03:02 -0500

    ,

--- js/sandbox.js
+++ js/sandbox.js
@@ -6,6 +6,32 @@ let output = document.getElementById("output");
 
 submitBtn.addEventListener("click", getInfo);
 
-  function getInfo() { 
-    output.textContent = `Name: ${nameInput.value}, Age: ${ageInput.value}, Favorite Color: ${colorInput.value}';
+
+let pet = document.querySelectorAll('input[type=radio]');
+let hobby = document.querySelectorAll('input[type="checkbox"]');
+
+let favPet;
+let hobbies = [];
+
+function getInfo(){
+  //reset hobbies Array each time value
+  hobbies = [];
+  
+  //Check for Pets
+  for(let i = 0; i <pet.length; i++){
+    if(pet[i].checked){
+      favPet = pet[i].value;
     }
+  }
+  console.log(favPet);
+
+  //check hobbies
+  for(let i = 0; i < hobby.length; i++){
+    if(hobby[i].checked){
+      hobbies.push(" " + hobby[i].value);
+    }
+  }
+  console.log(hobbies);
+  
+  output.textContent = 'name: ${nameInput.value}, age: ${ageInput.value}, Favorite Color: ${colorInput.value}';
+}
